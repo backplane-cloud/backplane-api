02b875c5eae7f2b24690329feba4c17280ddd1e7
Lewis Sheridan <lewis@backplane.cloud>
2023-08-17 01:33:22 +0100

    removed commented out code

--- backend/middleware/authzMiddleware.js
+++ backend/middleware/authzMiddleware.js
@@ -1,15 +1,4 @@
 import asyncHandler from "express-async-handler";
-// import { getUserInternal } from "../controllers/userController.js";
-// import {
-//   getUserAssignments,
-//   getTeamAssignments,
-//   getAssignments,
-// } from "../controllers/assignmentController.js";
-// import {
-//   getInternalRoles,
-//   getInternalRoleActions,
-// } from "../controllers/roleController.js";
-// import Team from "../models/teamModel.js";
 
 const authz = asyncHandler(async (req, res, next) => {
   console.time("AuthZ");
@@ -17,10 +6,7 @@ const authz = asyncHandler(async (req, res, next) => {
   console.log("AuthZ Middleware");
   console.log(`IDENTITY: ${req.user.orgId}\\${req.user.name}`);
 
-  // Get User Action being Performed
-  //console.log("baseurl:", req.baseUrl);
-
-  let action = req.baseUrl.substring(4, req.baseUrl.length);
+  let action = req.baseUrl.substring(4, req.baseUrl.length); // e.g. /products
   let requestedAction = req.baseUrl.substring(4, req.baseUrl.length);
   console.log(req.method, action);
 
@@ -66,178 +52,6 @@ const authz = asyncHandler(async (req, res, next) => {
     ? action.replace(`${requestedAction}/${req.params.id}`, "")
     : action.replace(`${requestedAction}`, "");
 
-  /*
-  // ORGS
-  if (action === "/orgs") {
-    if (req.method === "GET") {
-      action = req.params.id
-        ? `${action}/${req.params.id}/read`
-        : `${action}/${req.user.orgId}/read`;
-    }
-    if (req.method === "PUT") {
-      action = `${action}/write`;
-    }
-    if (req.method === "POST") {
-      action = "/"; //`${action}/${req.params.id}/write`;
-    }
-    if (req.method === "DELETE") {
-      action = `${action}/${req.params.id}/delete`;
-    }
-
-    inferredAction = action;
-  }
-
-  // PLATFORM
-  if (action === "/platforms") {
-    if (req.method === "GET") {
-      action = req.params.id
-        ? `/orgs/${req.user.orgId}${action}/${req.params.id}/read`
-        : `/orgs/${req.user.orgId}${action}/read`;
-    }
-    if (req.method === "PUT") {
-      action = `/orgs/${req.user.orgId}${action}/${req.params.id}/write`;
-    }
-    if (req.method === "POST") {
-      action = `/orgs/${req.user.orgId}${action}/write`;
-    }
-    if (req.method === "DELETE") {
-      action = `/orgs/${req.user.orgId}${action}/${req.params.id}/delete`;
-    }
-
-    inferredAction = req.params.id
-      ? action.replace(`/platforms/${req.params.id}`, "")
-      : action.replace(`/platforms`, "");
-    // inferredAction = [inferredAction, "/*"];
-  }
-
-  // PRODUCT
-  if (action === "/products") {
-    if (req.method === "GET") {
-      action = `/orgs/${req.user.orgId}${action}/read`;
-    }
-    if (req.method === "PUT") {
-      action = `/orgs/${req.user.orgId}${action}/${req.params.id}/write`;
-    }
-    if (req.method === "POST") {
-      action = `/orgs/${req.user.orgId}${action}/write`;
-    }
-    if (req.method === "DELETE") {
-      action = `/orgs/${req.user.orgId}${action}/${req.params.id}/delete`;
-    }
-
-    inferredAction = action.replace(`/products`, "");
-  }
-
-  if (action === "/apps") {
-    if (req.method === "GET") {
-      action = req.params.id
-        ? `/orgs/${req.user.orgId}${action}/${req.params.id}/read` // Fully Qualified Action
-        : `/orgs/${req.user.orgId}${action}/read`;
-    }
-    if (req.method === "PUT") {
-      action = `/orgs/${req.user.orgId}${action}/${req.params.id}/write`;
-    }
-    if (req.method === "POST") {
-      action = `/orgs/${req.user.orgId}${action}/write`;
-    }
-    if (req.method === "DELETE") {
-      action = `/orgs/${req.user.orgId}${action}/${req.params.id}/delete`;
-    }
-
-    inferredAction = req.params.id
-      ? action.replace(`/apps/${req.params.id}`, "")
-      : action.replace(`/apps`, "");
-  }
-
-  // REQUESTS
-  if (action === "/requests") {
-    if (req.method === "GET") {
-      action = req.params.id
-        ? `/orgs/${req.user.orgId}${action}/${req.params.id}/read`
-        : `/orgs/${req.user.orgId}${action}/read`;
-    }
-    if (req.method === "PUT") {
-      action = `/orgs/${req.user.orgId}${action}/${req.params.id}/write`;
-    }
-    if (req.method === "POST") {
-      action = `/orgs/${req.user.orgId}${action}/write`;
-    }
-    if (req.method === "DELETE") {
-      action = `/orgs/${req.user.orgId}${action}/${req.params.id}/delete`;
-    }
-
-    inferredAction = req.params.id
-      ? action.replace(`/requests/${req.params.id}`, "")
-      : action.replace(`/requests`, "");
-  }
-  // SERVICES
-  if (action === "/services") {
-    if (req.method === "GET") {
-      action = req.params.id
-        ? `/orgs/${req.user.orgId}${action}/${req.params.id}/read`
-        : `/orgs/${req.user.orgId}${action}/read`;
-    }
-    if (req.method === "PUT") {
-      action = `/orgs/${req.user.orgId}${action}/${req.params.id}/write`;
-    }
-    if (req.method === "POST") {
-      action = `/orgs/${req.user.orgId}${action}/write`;
-    }
-    if (req.method === "DELETE") {
-      action = `/orgs/${req.user.orgId}${action}/${req.params.id}/delete`;
-    }
-
-    inferredAction = req.params.id
-      ? action.replace(`/services/${req.params.id}`, "")
-      : action.replace(`/services`, "");
-  }
-*/
-
-  // //console.log("Action:", action);
-  // const uid = req.user.id;
-  // const orgid = req.user.orgId;
-
-  // // Get User Assignments
-  // const userAssignments = await getUserAssignments({ id: uid, orgId: orgid });
-  // //console.log(`User Assignments: ${userAssignments}`);
-
-  // // Get User Actions
-  // const userActions = await Promise.all(
-  //   userAssignments.map(async (assignment) =>
-  //     getInternalRoleActions(assignment.role, assignment.scope, orgid)
-  //   )
-  // );
-  // //console.log(userActions);
-
-  // // Get Team Assignments
-  // const teamAssignments = await getTeamAssignments();
-  // //console.log(teamAssignments);
-
-  // // Filter the Teams to those that the User is a member of
-  // const filteredTeams = teamAssignments.filter(
-  //   (assignment) => {
-  //     return req.user.teams.includes(assignment.principal);
-  //   } //&& assignment
-  // );
-  // //console.log(`Team Assignments: ${filteredTeams}`);
-
-  // // Get Team Actions
-  // const filteredRoles = await Promise.all(
-  //   filteredTeams.map(async (assignment) =>
-  //     getInternalRoleActions(assignment.role, assignment.scope, orgid)
-  //   )
-  // );
-  // //console.log("Team Actions:", filteredRoles);
-
-  // // Merge Actions
-  // const allActions = [...userActions, ...filteredRoles];
-
-  // const allowedActions = allActions.flat();
-  // // const newAllowedActions = allowedActions.map((action) =>
-  // //   action != "*" ? `/orgs/${req.user.orgId}${action}` : "*"
-  // // );
-  // //console.log(actions);
-
   console.log("Requested Action:", action);
   console.log("Inferred Allowed Action:", inferredAction);
   console.log("Actions Allowed:", req.user.allowedActions);
@@ -249,11 +63,6 @@ const authz = asyncHandler(async (req, res, next) => {
     req.user.allowedActions.includes(inferredAction)
       ? true
       : false;
-
-  // Restrict Org Read to same org unless /orgs/read is present
-  // if (!allowedActions.includes("/orgs/read")) {
-  //   req.params.id = req.user.orgId;
-  // }
 
   if (authorised) {
     console.log(`Authorisation Successful for ${req.user.name} at ${action}`);
