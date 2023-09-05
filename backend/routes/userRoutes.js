c38ed8d4b2c10c5e43e5ffb41679b0720cbe5db3
Lewis Sheridan <lewis@backplane.cloud>
2023-09-05 04:45:59 +0100

    added register, login, delete to openapi

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -29,6 +29,17 @@
  *           type: string
  *           format: password
  *           description: User Password
+ *         example:
+ *           name: Lewis Sheridan
+ *           email: lewis@backplane.cloud
+ *           password: mypassword
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: User
+ *   description: Organisation Users
  */
 
 /**
@@ -38,6 +49,7 @@
  *    security:
  *      - bearerAuth: []
  *    summary: Returns all users in the Org
+ *    tags: [User]
  *    responses:
  *      200:
  *        description: List of all users
@@ -48,6 +60,127 @@
  *              items:
  *                $ref: '#/components/schemas/User'
  *
+ */
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Get the User by ID
+ *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The User ID
+ *     responses:
+ *       200:
+ *         description: The book description by id
+ *         contents:
+ *           application/json:
+ *             schema:
+ *              $ref: '#/components/schemas/User'
+ *       404:
+ *         description: The User was not found
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Creates a new User within an Organisation
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The book was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schema/User'
+ *       500:
+ *         description: Server Error
+ */
+
+/**
+ * @swagger
+ * /users/register:
+ *   post:
+ *     summary: Registers a new User and Creates a new Organisation
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The user was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schema/User'
+ *       500:
+ *         description: Server Error
+ */
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   delete:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Deletes a User by ID
+ *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The User ID
+ *     responses:
+ *       200:
+ *         description: The user description by id
+ *         contents:
+ *           application/json:
+ *             schema:
+ *              $ref: '#/components/schemas/User'
+ *       404:
+ *         description: The User was not found
+ */
+
+/**
+ * @swagger
+ * /users/login:
+ *   post:
+ *     summary: Logs in a User
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The user has successfully authenticated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schema/User'
+ *       500:
+ *         description: Server Error
  */
 
 import express from "express";
@@ -71,8 +204,11 @@ const router = express.Router();
 router.route("/me").get(protect, authz, getMe).put(protect, authz, updateMe);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
-router.route("/").post(registerUser).get(protect, authz, getUsers);
-router.route("/create").post(protect, authz, createUser);
+router.route("/register").post(registerUser);
+router
+  .route("/")
+  .post(protect, authz, createUser)
+  .get(protect, authz, getUsers);
 
 router
   .route("/:id")
