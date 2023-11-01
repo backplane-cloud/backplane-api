23cdf3f0ee495821df5bd87b3a80ba29afd1b50a
Lewis Sheridan <lewis@backplane.cloud>
2023-11-01 20:14:18 +0000

    Added parentId to BacklogItem

--- backend/controllers/backlogController.js
+++ backend/controllers/backlogController.js
@@ -150,6 +150,7 @@ const setBacklogItem = asyncHandler(async (req, res) => {
     sprint: req.body.sprint,
     productId: req.body.productId,
     backlogId: req.params.id,
+    parentId: req.body.parentId,
   });
   console.log(req.body);
   res.status(200).json(backlogItem);
